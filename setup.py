cd772e9424f4a35b1de304f2cf2556c85a9bd098
Kento Masui <nameissoap@gmail.com>
2016-04-10 21:10:09 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+    name="proboscuis_util",
+    version="1.0",
+    description="proboscis's utility library",
+    author="Kento Masui",
+    author_email="nameissoap@gmail.com",
+    packages=find_packages(include="proboscis"))
